36a5c83bc0094b32259280d428029d62d449ee53
Alex Karadimos <karadalex@gmail.com>
2018-06-18 10:41:14 +0300

    get data from pad train on browser with TensorflowJS (TODO) and store trained model on blockchain

--- dapp/js/signature.js
+++ dapp/js/signature.js
@@ -5,3 +5,30 @@ var signaturePad = new SignaturePad(canvas, {
     // this option can be omitted if only saving as PNG or SVG
     backgroundColor: 'rgb(255, 255, 255)'
 });
+
+
+// Start training from the new data
+$('#train').on('click', function(event) {
+    // Get the data from the signature pad
+    // I use 'toData()' method because it returns much richer information
+    // for the signature, than a picture, i.e. points in time, when mouse
+    // was lifted and then down again etc. => More features for 
+    // the Machine Learning
+    var padData = signaturePad.toData();
+
+    // Do the training here using the data above
+
+
+    // Upload trained model to Blockchain
+    var signatureInstance;
+
+    contracts.Signature.deployed().then(function(instance) {
+        signatureInstance = instance;
+
+        return signatureInstance.trainSignatureModel(padData.toString(), {from: account});
+    }).then(function(result) {
+        console.log(result);
+    }).catch(function(err) {
+        console.log(err.message);
+    });
+})
