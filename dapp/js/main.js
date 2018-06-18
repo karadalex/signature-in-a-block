f44205f959639123c716c6438245e3ebba11088b
Alex Karadimos <karadalex@gmail.com>
2018-06-18 10:40:00 +0300

    load Signature contract ABI (built automatically from truffle)

--- dapp/js/main.js
+++ dapp/js/main.js
@@ -1,3 +1,5 @@
+var contracts = {};
+
 // Is there an injected web3 instance?
 if (typeof web3 !== 'undefined') {
     web3 = new Web3(web3.currentProvider);
@@ -5,4 +7,13 @@ if (typeof web3 !== 'undefined') {
     // If no injected web3 instance is detected, fall back to Ganache
     web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
 }
+
+$.getJSON('Signature.json', function(data) {
+    // Get the necessary contract artifact file and instantiate it with truffle-contract
+    var SignatureArtifact = data;
+    contracts.Signature = TruffleContract(SignatureArtifact);
+
+    // Set the provider for our contract
+    contracts.Signature.setProvider(web3Provider);
+});
 
