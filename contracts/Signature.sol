69d789547bcc1795752c3f7b1fe3aa1a956fa4f9
Alex Karadimos <karadalex@gmail.com>
2018-06-18 05:21:37 +0300

    Basic contract to save and update signature model

---
+++ contracts/Signature.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.24;
+
+contract Signature {
+    
+    struct mySignature {
+        string signaturePublicKey;
+        string signatureTrainedMLModel;
+    }
+
+    mapping (address => mySignature) signatures;
+
+    function trainSignatureModel(string newTrainedModel) public {
+        mySignature storage sign = signatures[msg.sender];
+        sign.signatureTrainedMLModel = newTrainedModel;
+    }
+
+    function saveSignaturePublicKey(string _publicKey) public {
+        mySignature storage sign = signatures[msg.sender];
+        sign.signaturePublicKey = _publicKey;
+    }
+}
